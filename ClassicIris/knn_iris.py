4e788505aa3bb3a7e4d1f7c4213e75692cae70ad
Anthony Morast <anthonymorast33@hotmail.com>
2017-03-02 21:51:23 -0700

    Added iris data and iris data anlysis

---
+++ ClassicIris/knn_iris.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import random
+
+cwd = os.getcwd()
+sys.path.append(cwd + '/../common/')
+import data_controller as dc
+import classify as clsfy
+
+
+# CLASSES
+IRIS_SETOSA = 0
+IRIS_VERSICOLOR = 1
+IRIS_VIRGINICA = 2
+
+
+'''
+	Converts the classification (a string) to a numeric value.
+'''
+def ClassToNumeric(data):
+	for key in data:
+		if data[key][0] == 'Iris-virginica':
+			data[key][0] = IRIS_VIRGINICA
+		elif data[key][0] == 'Iris-versicolor':
+			data[key][0] = IRIS_VERSICOLOR
+		elif data[key][0] == 'Iris-setosa':
+			data[key][0] = IRIS_SETOSA
+			
+	return data
+
+if __name__ == "__main__":
+	filename = "Iris.csv"
+	data = dc.get_data(filename)
+	neighbors = 5
+
+	# in this case, to shuffle the data we added a column in Excel that was just '=rand()'
+	# dragging this column down prroduced a random number for each row, sorting on this
+	# random column shuffled the data to be better split for test/train. Here we're removing
+	# this dummy column from our model. Also modified CSV so classes are in column 0
+	for key in data:
+		row = data[key]
+		data[key] = row[:-1]
+
+	data = ClassToNumeric(data)
+
+	train = dict()
+	for k, v in list(data.items())[:-130]:
+		train[k] = v
+
+	test = dict()
+	for k, v in list(data.items())[-130:]:
+		test[k] = v
+	
+	right = 0
+	wrong = 0
+	correct_avg_dists = []
+	correct_min_dists = []
+	incorrect_min_dists = []
+	incorrect_avg_dists = []
+	for key in test:
+		row = test[key]
+		actual_class = row[0]
+		class_data = clsfy.knn(row[1:], train, 3, neighbors)
+		predicted_class = class_data[0]
+		if actual_class != predicted_class:
+			wrong += 1
+			incorrect_min_dists.append(class_data[1])
+			incorrect_avg_dists.append(class_data[2])
+		else:
+			right += 1
+			correct_min_dists.append(class_data[1])
+			correct_avg_dists.append(class_data[2])
+
+
+	print('\n\nNeighbors: ', neighbors)
+
+	print("\nCorrect: ", right, '/', len(test), ' = ', right/len(test))
+	print("Wrong: ", wrong, '/', len(test), ' = ', wrong/len(test))
+
+	print('\nIncorrect Average Min Distance: ', sum(incorrect_min_dists)/len(incorrect_min_dists))
+	print('Incorrect Average Average Distance: ', sum(incorrect_avg_dists)/len(incorrect_avg_dists))
+
+	print('\nCorrect Average Min Distance: ', sum(correct_min_dists)/len(correct_min_dists))
+	print('Correct Average Min Distance: ', sum(correct_avg_dists)/len(correct_avg_dists))
--- LendingClub/knn_lc.py
+++ LendingClub/knn_lc.py
@@ -1,10 +1,12 @@
-import data_controller as dc
-from math import *
-import threading
-from queue import Queue
 import random
-import pylab as plt
-import sys 
+import os
+import sys
+
+cwd = os.getcwd()
+sys.path.append(cwd + '/../common/')
+import data_controller as dc
+import classify as clsfy
+
 
 ### GLOBAL STRING TO INT CONSTANTS
 # NaN
@@ -86,8 +88,6 @@ def removeBadRows(data_dict):
 
 	for key in data_dict:
 		row = data_dict[key]
-		if row[TOTAL_ACC] is None:
-			bad_keys.append(key)
 		try:
 			float(row[LOAN_AMNT])
 			float(row[DTI])
@@ -198,205 +198,28 @@ def convertStringsToNumeric(data_dict):
 	
 	return data_dict
 
-'''
-	Normalizes the values so that each feature has an equal vote on 
-	'closeness'. Convert data to values between 0 and 1
-'''
-def normalize(data_dict):
-	maxes = dict()
-	
-	# known/defined
-	maxes[GRADE] = 6
-	maxes[EMP_LENGTH] = 10
-	maxes[TERM] = 60
-	maxes[HOME_OWNERSHIP] = 4
 
-	# unknown
-	maxes[DTI] = 0
-	maxes[INT_RATE] = 0
-	maxes[ANNUAL_INC] = 0
-	maxes[LOAN_AMNT] = 0
-	maxes[DELINQ_2YRS] = 0
-	maxes[INSTALLMENT] = 0
-	maxes[INQ_LAST_6MTHS] = 0
-	maxes[OPEN_ACC] = 0
-	maxes[PUB_REC] = 0
-	maxes[REVOL_BAL] = 0
-	maxes[TOTAL_ACC] = 0
+#######################################################################################################################
 
-	for key in data_dict:
-		row = data_dict[key]
-		for i in range(1, 16):
-			if float(row[i]) > maxes[i]:
-				maxes[i] = float(row[i])
 
-	for key in data_dict:
-		row = data_dict[key]
-		# normalize the values row[val] /= maxes[val]
-		for i in range(1, 16):
-			row[i] = float(row[i]) / maxes[i]
-
-	return data_dict
-
-'''
-	Returns the Euclidian distance between two vectors
-
-	x_1, x_2 - vectors (lists of values, must have same dimensions)
-
-	NOTES: slightly test, should be correct
-'''
-def euclidianDistance(x_1, x_2):
-	e = 0
-	# start at 1 to skip the classification 
-	for i in range(len(x_1)):
-		e += (float(x_1[i]) - float(x_2[i]))**2
-	return sqrt(e)
-
-'''
-	Takes a new vector of data and a training (test) set and returns a classification of the new data. 
-	Compares the new vector with each test vector finding the k nearest neighbers, averages the classification, 
-	of the nearest neighbors, and assigns that classification.
-'''
-def knn(x_t, data_dict, k=5):
-	# large values for original neighbors [classification, distance]
-	nn = []
-	for i in range(k):
-		nn.append([100, 999999999])
-
-	# find nearest neighbors	
-	for key in data_dict:
-		row = data_dict[key]
-		dist = euclidianDistance(row[1:], x_t)
-		for d in nn:
-			if dist < d[1]:
-				d[1] = dist
-				d[0] = row[LOAN_STATUS]
-				break
-	
-	#print(nn)
-	# average nearest neighbors classification
-	ones = 0
-	zeros = 0
-	min_dist = 999999999
-	avg_dist = 0
-	for n in nn:
-		# weighted distance, the nearest neighbors with shortest distances count more
-		# this weights the votes, not the features
-		if n[0] == 1:
-			ones += n[1]
-		else:
-			zeros += n[1]
-		avg_dist += n[1]
-		if n[1] < min_dist:
-			min_dist = n[1]
-			
-	# want the ones/zeros with the smallest distance (these nodes were typically closer to x_t)
-	if ones < zeros:
-		certainty = ones/k
-		return 1, certainty, min_dist, avg_dist
-	else:
-		certainty = zeros/k
-		return 0, certainty, min_dist, avg_dist
-
-'''
-	Runs the program for a certain subset of the test_data.
-	Mainly used for testing, when in production, very few vectors will be passed to the program.
-'''
-def worker(test_data, train_data, k=5):
-	right = 0
-	wrong = 0
-	
-	for key in test_data:
-		row = test_data[key]
-		actual = row[LOAN_STATUS]
-		if knn(row[1:], train_data) == actual:
-			right += 1
-		else:
-			wrong += 1
-		print(key)
-	q.put(right, wrong)
-
-'''
-	Tests the classifier using another data set.
-'''
-def test(train_data, test_data):
-	total_cases = len(test_data)
-	num_threads = 12
-	dict_args = []
-	rows_per_thread = int(len(test_data) / num_threads)
-	for i in range(num_threads - 1):
-		d = dict()
-		start = i * rows_per_thread
-		end = (start + rows_per_thread) - 1
-		for k, v in list(test_data.items())[start:end]:
-			d[k] = v
-		dict_args.append(d)
-
-	start = 7 * rows_per_thread
-	d = dict()
-	for k, v in list(test_data.items())[start:]:
-		d[k] = v
-	dict_args.append(d)
-		
-
-	# [:] makes a copy of a list by value
-	right = 0
-	wrong = 0
-
-	q = Queue()
-	threads = []
-	for i in range(num_threads):
-		t = threading.Thread(target=worker, args=(dict_args[i], train_data), name=str(i))
-		t.daemon = True
-		t.start()
-		threads.append(t)	
-
-	print(len(threads))
-
-	for t in threads:
-		t.join()
-
-	print(q)
-
-	for r in q:
-		print(r)
-		right += r[0]
-		wrong += r[1]
-
-	print("Right: ", right/total_cases)
-	print("Wrong: ", wrong/total_cases)
-
-'''
-	Returns the training and test data.
-'''
-def getData(test_data='LoanStats3b.csv', train_data='train_data.csv'):
+if __name__ == '__main__':
 	cols = ['loan_status', 'loan_amnt', 'home_ownership', 'dti', 'int_rate', 'annual_inc', 'grade', 'emp_length', 'delinq_2yrs',
 			'term', 'installment', 'inq_last_6mths', 'open_acc', 'pub_rec', 'revol_bal', 'total_acc']
 
 	print('Getting test data...')
-	test_data = dc.get_data(test_data, prune_cols=cols)
+	test_data = dc.get_data('LoanStats3b.csv', prune_cols=cols)
 	test_data = intRateToNumeric(test_data)
 	test_data = convertStringsToNumeric(test_data)
 	test_data = removeBadRows(test_data)
 
 	print('\nGetting train data...')
-	train_data = dc.get_data(train_data, prune_cols=cols)
+	train_data = dc.get_data('train_data.csv', prune_cols=cols)
 	train_data = convertStringsToNumeric(train_data)
 	train_data = intRateToNumeric(train_data)
 	train_data = removeBadRows(train_data)
 
-	print("\nNormalizing data...")
-	train_data = normalize(train_data)
-	test_data = normalize(test_data)
-
-	return test_data, train_data
-
-
-'''
-	Runs the classifier.
-'''
-def run(cases, neighbors, test_data, train_data):
-	# get cases  
+	# get 100 rows 
+	cases = 300
 	start = random.randint(0, len(test_data) - (cases + 1))
 	examples = dict()
 	for k, v in list(test_data.items())[start:start+cases]:
@@ -417,19 +240,19 @@ def run(cases, neighbors, test_data, train_data):
 	for key in examples:
 		row = examples[key]
 		actual = row[0]
-		classification = knn(row[1:], train_data, neighbors)
+		classification = clsfy.knn(row[1:], train_data, 2, 3)
 		if classification[0] == 0:
 			negatives += 1
 		else:
 			positives += 1
 		if  classification[0] == actual:
 			right += 1
-			#print("Correct classification. Average Distance: ", classification[3])
+			print("Correct classification. Certainty: ", classification[1])
 			correct_certain.append(classification[1])
 			correct_min_dist.append(classification[2])
 			correct_avg_dist.append(classification[3])
 		else:
-			#print("Incorrect classification. Average Distance: ", classification[3], ' Actual: ', actual)
+			print("Incorrect classification. Certainty: ", classification[1], ' Actual: ', actual)
 			incorrect_certain.append(classification[1])
 			incorrect_min_dist.append(classification[2])
 			incorrect_avg_dist.append(classification[3])
@@ -450,45 +273,3 @@ def run(cases, neighbors, test_data, train_data):
 
 	print("\nCorrect Avg Distance Average: ", sum(correct_avg_dist)/len(correct_avg_dist))
 	print("Incorrect Avg Distance Average: ", sum(incorrect_avg_dist)/len(incorrect_avg_dist))
-
-	# c_run = [i for i in range(len(correct_avg_dist))]
-	# i_run = [i for i in  range(len(incorrect_avg_dist))]
-	
-	# plt.plot(c_run, correct_avg_dist, 'g.')
-	# plt.plot(i_run, incorrect_avg_dist, 'r.')
-	# plt.show()
-	return [right/cases, wrong/cases]
-
-'''
-	Entry of the program.
-
-	Takes two parameters 'python3 knn_classifier.py <runs> <neighbors> <cases>' 
-	OR 
-	no paramters, defaults will be used:
-		runs = 10
-		neighbors = 5
-		cases = 100
-'''
-if __name__ == '__main__':
-	test_data, train_data = getData()
-
-	if len(sys.argv) < 4:
-		runs = 10
-		neighbors = 5
-		cases = 100
-	else:
-		runs = int(sys.argv[1])
-		neighbors = int(sys.argv[2])
-		cases = int(sys.argv[3])
-
-	right = []
-	wrong = []
-	for i in range(1, runs + 1):
-		print('\nProcessing run ' + str(i) + ' of ' + str(runs))
-		r, w = run(cases, neighbors, test_data, train_data)
-		right.append(r)
-		wrong.append(w)
-
-	print(right)
-	print(wrong)
-	
