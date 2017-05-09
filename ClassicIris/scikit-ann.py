69ddc5468f659454bf73e5e9c207266e0666ccf3
Anthony Morast <anthonymorast33@hotmail.com>
2017-05-08 23:40:51 -0600

    functioning scikit learn ann regressor, less than 100 test cases overfits and provides terrible R squared. 100 test 50 train seems to do well

--- ClassicIris/scikit-ann.py
+++ ClassicIris/scikit-ann.py
@@ -1,6 +1,5 @@
+from sklearn.neural_network import MLPClassifier, MLPRegressor
 import numpy as np
-from scipy import optimize
-import matplotlib.pyplot as plt 
 
 import sys
 import os
@@ -8,55 +7,23 @@ cwd = os.getcwd()
 sys.path.append(cwd + '/../common/')
 import data_controller as dc
 
-from trainer import *
-from ann import *
-
 DATA_FILE_NAME = 'Iris.csv'
 IRIS_SETOSA = [0, 'Iris-setosa']
 IRIS_VERSICOLOR = [1, 'Iris-versicolor']
 IRIS_VIRGINICA = [2, 'Iris-virginica']
 
-def computeNumericalGradient(N, X, y):
-	paramsInitial = N.getParams()
-	numgrad = np.zeros(paramsInitial.shape)
-	perturb = np.zeros(paramsInitial.shape)
-	e = 1e-4
-
-	for p in range(len(paramsInitial)):
-		perturb[p] = e
-		N.setParams(paramsInitial + perturb)
-		loss2 = N.cost(X, y)
-
-		N.setParams(paramsInitial - perturb)
-		loss1 = N.cost(X, y)
-
-		numgrad[p] = (loss2 - loss1) / (2*e)
-		perturb[p] = 0
-
-	N.setParams(paramsInitial)
-	return numgrad
-
-# x = np.array(([3,5], [5,1], [10,2]), dtype=float)
-# y = np.array(([75], [82], [93]), dtype=float)
-
-# # scale the data
-# x = x/np.max(x, axis=0)
-# y = y/100 # test scores in this case so max is 100
-# eta = 3
-
 data = dc.get_data(DATA_FILE_NAME)
-NN = NeuralNetwork(4, 1, 5)
-testSize = 50
+testSize = 100
 
 y = []
 for key in data:
 	row=data[key]
 	if row[0] in IRIS_SETOSA:
-		y.append(IRIS_SETOSA[0]/2)
+		y.append(IRIS_SETOSA[0])
 	elif row[0] in IRIS_VIRGINICA:
-		y.append(IRIS_VIRGINICA[0]/2)
+		y.append(IRIS_VIRGINICA[0])
 	elif row[0] in IRIS_VERSICOLOR:
-		y.append(IRIS_VERSICOLOR[0]/2)
+		y.append(IRIS_VERSICOLOR[0])
 
 	data[key] = data[key][1:]
 	data[key] = data[key][:-1]
@@ -73,8 +40,7 @@ test = dict()
 for k, v in list(data.items())[-testSize:]:
 	test[k] = v
 
-dy = y[:-testSize]
-trainY = np.array([[i] for i in dy])
+trainY = y[:-testSize]
 
 rows = []
 for k in train:
@@ -92,18 +58,16 @@ for k in test:
 testX = np.array([row for row in rows])
 testX = testX/np.max(testX, axis=0)
 
-dy = y[-testSize:]
-testY = np.array([[i] for i in dy])
+testY = y[-testSize:]
 
-T = trainer(NN)
-T.train(trainX, trainY)
+x=[[0, 0], [1., 1.]]
+y = [0, 1]
 
-# plt.plot(T.E)
-# plt.grid(1)
-# plt.xlabel('Iterations')
-# plt.ylabel('Cost')
-# plt.show()
+clf = MLPRegressor(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5,2), random_state=1)
 
+clf.fit(trainX, trainY)
+print(clf.score(testX, testY))
+yHats = clf.predict(testX)
 
-print(NN.forward(testX))
+print(np.round(yHats))
 print(testY)
