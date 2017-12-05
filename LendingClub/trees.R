75522eb78da290c738f7516caf5abf25ec9e7c1c
Anthony <anthonymorast33@hotmail.com>
2017-12-05 13:20:36 -0700

    removed duplicate dirs after restructre

--- LendingClub/trees.R
+++
@@ -1,2 +0,0 @@
-data <- read.csv("LoanStats3a.csv")
-
--- WorldHappiness/worldHappiness.R
+++
@@ -1,8 +0,0 @@
-wh <- read.csv("world_happiness2016.csv")
-wh.features <- wh[, -c(1, 2, 3, 4, 5, 6)]
-
-# make a variable where the happiest half = 1, least happy half = 0
-wh.features$Happy.Class <- factor(ifelse(wh$Happiness.Rank <= 79, 1, 0))
-wh.tree <- rpart(Happy.Class ~ ., data=wh.features)
-fancyRpartPlot(wh.tree)
-
